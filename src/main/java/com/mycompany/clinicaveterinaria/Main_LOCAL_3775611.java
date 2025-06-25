48d88eeb3d89e854cbbcf9facd4680377fc5261c
Caliel Oliveira Albuquerque <caliel.o.albuquerque@outlook.com>
2025-06-25 17:54:59 -0300

    Removendo arquivos gerados pela mesclagem

--- src/main/java/com/mycompany/clinicaveterinaria/Main_LOCAL_3775611.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-import java.awt.*;
-
-public class Main {
-    public static void main(String args[]) {
-        Clinica clinica = new Clinica();
-        UI ui = new UI(clinica);
-    }
-}
